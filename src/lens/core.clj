5c129dc2f9c9c2dc238689860423e54f3a964f5d
Alexander Kiel <alexanderkiel@gmx.net>
2015-05-13 10:55:52 +0200

    Improve CLI Options Handling

--- src/lens/core.clj
+++ src/lens/core.clj
@@ -1,30 +1,61 @@
 (ns lens.core
-  (:require [clojure.tools.cli :as cli]
+  (:require [clojure.string :as str]
+            [clojure.tools.cli :as cli]
             [org.httpkit.server :refer [run-server]]
             [lens.app :refer [app]]
             [lens.util :refer [parse-int]]))
 
-(defn cli [args]
-  (cli/cli args
-    ["-p" "--port" "Listen on this port" :default 8080 :parse-fn parse-int]
-    ["-i" "--ip" "The IP to bind" :default "0.0.0.0"]
-    ["-t" "--thread" "Number of worker threads" :default 4 :parse-fn parse-int]
-    ["-d" "--database-uri" "The Datomic database URI to use"]
-    ["--token-introspection-uri" "The OAuth2 token inspection URI to use"]
-    ["-c" "--context-path" "An optional context path under which the workbook service runs. Has to start and end with a slash." :default "/"]))
+(def cli-options
+  [["-p" "--port PORT" "Listen on this port" :default 8080 :parse-fn parse-int
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-i" "--ip IP" "The IP to bind" :default "0.0.0.0"]
+   ["-t" "--thread NUM" "Number of worker threads" :default 4 :parse-fn parse-int
+    :validate [#(< 0 % 64) "Must be a number between 0 and 64"]]
+   ["-d" "--database-uri URI" "The Datomic database URI to use"
+    :validate [#(.startsWith % "datomic")
+               "Database URI has to start with datomic."]]
+   [nil "--token-introspection-uri URI" "The OAuth2 token inspection URI to use"]
+   ["-c" "--context-path PATH" (str "An optional context path under which the "
+                                    "workbook service runs. Has to start and end "
+                                    "with a slash.") :default "/"]
+   ["-h" "--help" "Show this help"]])
+
+(defn usage [options-summary]
+  (->> ["Usage: lens-workbook [options]"
+        ""
+        "Options:"
+        options-summary
+        ""]
+       (str/join "\n")))
+
+(defn error-msg [errors]
+  (str/join "\n" errors))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
 
 (defn -main [& args]
-  (let [[options _ banner] (cli args)]
-    (if (and (:database-uri options) (:token-introspection-uri options))
-      (do
-        (run-server (app (:database-uri options)
-                         (:token-introspection-uri options)
-                         (:context-path options))
-                    (merge {:worker-name-prefix "http-kit-worker-"} options))
-        (println "Datomic:" (:database-uri options))
-        (println "OAuth2:" (:token-introspection-uri options))
-        (println "Context Path:" (:context-path options))
-        (println "Server started")
-        (println "Listen at" (str (:ip options) ":" (:port options)))
-        (println "Using" (:thread options) "worker threads"))
-      (println banner))))
+  (let [{:keys [options errors summary]} (cli/parse-opts args cli-options)]
+    (cond
+      (:help options)
+      (exit 0 (usage summary))
+
+      errors
+      (exit 1 (error-msg errors))
+
+      (nil? (:database-uri options))
+      (exit 1 "Missing database URI.")
+
+      (nil? (:token-introspection-uri options))
+      (exit 1 "Missing OAuth2 token inspection URI."))
+
+    (let [{:keys [database-uri token-introspection-uri context-path]} options]
+      (run-server (app database-uri token-introspection-uri context-path)
+                  (merge {:worker-name-prefix "http-kit-worker-"} options))
+      (println "Datomic:" database-uri)
+      (println "OAuth2:" token-introspection-uri)
+      (println "Context Path:" context-path)
+      (println "Server started")
+      (println "Listen at" (str (:ip options) ":" (:port options)))
+      (println "Using" (:thread options) "worker threads"))))
