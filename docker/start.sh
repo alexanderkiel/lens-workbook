b1fee1c9b8632345e997fd3d2eef807ba410ad06
Alexander Kiel <alexanderkiel@gmx.net>
2015-10-22 10:00:50 +0200

    Respect DATOMIC_EDITION in start.sh
    Same as in Procfile.

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-lein with-profile production,datomic-free trampoline run -p 80 -d ${DB_URI:-datomic:free://db:4334/lens-workbook} -c ${CONTEXT_PATH:-/} --token-introspection-uri ${TOKEN_INTROSPECTION_URI:-http://auth/auth/introspect}
+lein with-profile production,datomic-${DATOMIC_EDITION:-free} trampoline run -p 80 -d ${DB_URI:-datomic:free://db:4334/lens-workbook} -c ${CONTEXT_PATH:-/} --token-introspection-uri ${TOKEN_INTROSPECTION_URI:-http://auth/auth/introspect}
