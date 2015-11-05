dbdf94e5f802abd97108c61e915c23f25a22d53b
Alexander Kiel <alexanderkiel@gmx.net>
2015-11-05 21:37:55 +0100

    Add Duration to Request/Response Logging

--- src/lens/app.clj
+++ src/lens/app.clj
@@ -24,12 +24,18 @@
       resp
       {:status 404})))
 
+(defn- duration [start end]
+  (Math/round (/ (double (- end start)) 1000000)))
+
 (defn wrap-log [handler]
   (fn [req]
-    (let [resp (handler req)]
+    (let [start (System/nanoTime)
+          resp (handler req)
+          end (System/nanoTime)]
       (log/info (pr-str {:request (select-keys req [:remote-addr :request-method
                                                     :uri :query-string :headers])
-                         :response (select-keys resp [:status :headers])}))
+                         :response (select-keys resp [:status :headers])
+                         :duration (str (duration start end) " ms")}))
       resp)))
 
 (defnk app [db-uri context-path :as opts]
